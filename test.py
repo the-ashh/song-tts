f58649e5eb90584194ae99cddbfca5e385fe77a3
cooler9711 <cooler9711@outlook.com>
2018-01-31 21:33:02 -0700

    init. probably gonna overwrite main.py

---
+++ test.py
@@ -0,0 +1,30 @@
+from bottle import Bottle, route, request, run
+
+
+@route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+def check_login(username, password):
+    if(username=="username" and password=="password"):
+        return True
+    else:
+        return False
+
+
+run(host='localhost', port=8080, debug=True)
