eeac0f5bbfa8492a54006863f0de8ffbe444b70b
cooler9711 <cooler9711@outlook.com>
2018-02-01 00:50:21 -0700

    i'm too tired to deal with this tonight

--- test.py
+++ test.py
@@ -4,54 +4,28 @@ from gtts import gTTS
 from tempfile import TemporaryFile
 import random
 import os
+import time
 
-filename = "mada.mp3"
-
-'''
-@route('/login', method='POST')
-def do_login():
-    name = request.forms.get('name')
-    artist = request.forms.get('artist')
-    lyrics = lyricwikia.get_lyrics(artist, name)
-    tts = gTTS(text=lyrics, lang='en', slow=False)
-    f = TemporaryFile()
-    tts.write_to_fp(f)
-    audio(random.randrange(999))
-'''
-
-@get("/static/css/<filepath:re:.*\.css>")
-def css(filepath):
-    return static_file(filepath, root="css")
 
 @route('/')
 def audio():
     return '''
-        <form action="/audio/''' + filename + '''"method="post">
+        <form action="/mada.mp3"method="post">
             Song Name: <input name="name" type="text" />
             Song Artist: <input name="artist" type="text" />
             <input value="Submit" type="submit" />
         </form>
     '''
 
-@route('/static/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='/path/to/your/static/files')
 
-@route('/audio/mada.mp3', method='POST')
-def getaudio(filepath):
+@route('/<filename>', method='POST')
+def getaudio(filename):
     name = request.forms.get('name')
     artist = request.forms.get('artist')
     lyrics = lyricwikia.get_lyrics(artist, name)
     tts = gTTS(text=lyrics, lang='en', slow=False)
-    tts.save('./audio/'+filename)
-    return static_file(filepath, root='./audio/'+filename, mimetype='audio/mpeg')
-    os.remove('./audio/'+filename)
-'''
-def check_login(username, password):
-    if(username=="username" and password=="password"):
-        return True
-    else:
-        return False
-'''
+    tts.save('./file.mp3')
+    return static_file(filename, root='./file.mp3', mimetype='audio/mpeg')
+
 
 run(host='localhost', port=8080, debug=True)
