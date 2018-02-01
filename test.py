6f0bc2018d206f2bd6b80ccfeef2d0c551a6521d
cooler9711 <cooler9711@outlook.com>
2018-01-31 23:53:00 -0700

    removed some stuff, have everything /almost/ working

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-from bottle import Bottle, route, request, static_file, run
+from bottle import Bottle, route, request, static_file, get, run
 import lyricwikia
 from gtts import gTTS
 from tempfile import TemporaryFile
 import random
 import os
+
+filename = "mada.mp3"
 
 '''
 @route('/login', method='POST')
@@ -17,32 +19,39 @@ def do_login():
     audio(random.randrange(999))
 '''
 
-@route('/audio')
+@get("/static/css/<filepath:re:.*\.css>")
+def css(filepath):
+    return static_file(filepath, root="css")
+
+@route('/')
 def audio():
     return '''
-        <form action="/audio" method="post">
+        <form action="/audio/''' + filename + '''"method="post">
             Song Name: <input name="name" type="text" />
             Song Artist: <input name="artist" type="text" />
             <input value="Submit" type="submit" />
         </form>
     '''
 
-@route('/audio', method='POST')
-def getaudio():
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='/path/to/your/static/files')
+
+@route('/audio/mada.mp3', method='POST')
+def getaudio(filepath):
     name = request.forms.get('name')
     artist = request.forms.get('artist')
     lyrics = lyricwikia.get_lyrics(artist, name)
     tts = gTTS(text=lyrics, lang='en', slow=False)
-    filename = str(random.randrange(999)) + '.mp3'
-    tts.save(filename)
-    return static_file(filename, root=filename, mimetype='audio/mpeg')
-    os.remove(filename)
-
+    tts.save('./audio/'+filename)
+    return static_file(filepath, root='./audio/'+filename, mimetype='audio/mpeg')
+    os.remove('./audio/'+filename)
+'''
 def check_login(username, password):
     if(username=="username" and password=="password"):
         return True
     else:
         return False
-
+'''
 
 run(host='localhost', port=8080, debug=True)
